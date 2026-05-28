88f7892519be877af9b4350b15ac9b350771e4e5
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-05-28 04:30:14 -0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("29777466", default=None, cast=int)
+API_HASH = config("a04b3df726520026f207079aec2f9879", default=None)
+BOT_TOKEN = config("8677808521:AAFOPTxHetOquXBBI5byQf6c7FRzsa440NY", default=None)
+SESSION = config("BQHGXjoABmMxqKLfb7AU86vPtLbNIBGttHEjSDCoV87Ph_UM_WRZ9ZdIxVcN0i4-6CckEo3qSXw7WRa_Zgj-OZjTVWtRtwFEpAmsvbhUHnQzv7u32dpENOs5DTR0mREjfdlPkcgbrQgLv6-BGcTiDgdmTriyYoigC-_gFcNaqYFkBcrtToBAjaf0XJDXgrxGXoeLAYk3PvgiGYw8HTH117tQ0jPpA7P63HTDNbopuL-CjyXlr-nURojIjAJiSGZofqqdRSlIpxg5knjTFE2ejorLsHitaBO7veaijkGg06yN-Yu7mrwX98ZyBfBq62rKK-wXQ5tR20ISEYcc8Qn07RuXGx6oowAAAAA1FSa2AA", default=None)
+FORCESUB = config("8399557684", default=None)
+AUTH = config("8399557684", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
